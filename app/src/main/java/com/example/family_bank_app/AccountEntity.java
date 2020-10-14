4f9f60d015b57da2c4dc8d5647027da03964b3c9
B.Foster <owensbrandan@gmail.com>
2020-10-13 19:20:36 -0700

    Added autoIncrement to primaryKey for AccountEntity

--- app/src/main/java/com/example/family_bank_app/AccountEntity.java
+++ app/src/main/java/com/example/family_bank_app/AccountEntity.java
@@ -9,7 +9,7 @@ import androidx.room.PrimaryKey;
 public class AccountEntity {
 
     // Fields for table with default values
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     @NonNull
     private int accountUid;
 
