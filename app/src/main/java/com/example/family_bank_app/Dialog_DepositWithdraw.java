2c6c1d7b3f162a27b44b9718f34daf00d30b447f
Theanticrumpet <dylan@rileyz.net>
2020-10-25 22:13:26 -0700

    begun deposit and withdraw dialog, renamed some button IDs

--- app/src/main/java/com/example/family_bank_app/Dialog_DepositWithdraw.java
+++ app/src/main/java/com/example/family_bank_app/Dialog_DepositWithdraw.java
@@ -1,4 +1,21 @@
 package com.example.family_bank_app;
 
-public class Dialog_DepositWithdraw {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class Dialog_DepositWithdraw extends AppCompatDialogFragment {
+
+    private String action;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle()
+
+        return builder.create();
+    }
 }
