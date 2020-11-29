c0281649ea13a2019f74f790e8fa9d5489725557
StopMakingUsernamesUnique <57193119+StopMakingUsernamesUnique@users.noreply.github.com>
2020-11-28 17:54:44 -0800

    Sample Data (No Labels)

--- app/src/main/java/com/example/family_bank_app/GraphActivity.java
+++ app/src/main/java/com/example/family_bank_app/GraphActivity.java
@@ -8,27 +8,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.*;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
+
 
 import java.util.List;
 
 public class GraphActivity extends AppCompatActivity {
-
-
+    LineGraphSeries<DataPoint> series;
+    double currentBal[]  = {3.00, 4.00};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
 
+        GraphView graphView = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries();
 
-        double currentBal[] = {3.00, 4.00}; //getIntent().getDoubleArrayExtra("BAL");
-        String date[] = {"3/20/2021", "3/21/2021"}; // getIntent().getStringArrayExtra("DATE");
+        double y, x;
+        x = -5.0;
+        for(int i = 0; i < 500; i++) {
+            x = x + .01;
+            y = Math.sin(x);
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
+         //getIntent().getDoubleArrayExtra("BAL");
+     /*   String date[] = {"3/20/2021", "3/21/2021"}; // getIntent().getStringArrayExtra("DATE");
 
         String hLabels[] = new String[date.length];
         String vLabels[] = new String[date.length];
-        GraphView.GraphViewData[] graphViewData = new GraphView.GraphViewData[date.length];
+        */
+
+        graphView.addSeries(series);
 
 
+
+        /*
         for(int i = 0; i < date.length; i++) {
             double z = currentBal[i];
             graphViewData[i] = (new DataPoint(Double.valueOf(i), z));
@@ -37,16 +50,20 @@ public class GraphActivity extends AppCompatActivity {
         }
 
         GraphViewSeries mSeries = new GraphViewSeries(graphViewData);
-        GraphView graphView = (GraphView) findViewById(R.id.graph);
-        graphView.addSeries(mSeries);
-        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
-            
-        });
-        graphView.setLa(hLabels);
-        graphView.setVerticalLabels(vLabels);
 
-        setContentView(R.layout.activity_graph);
+
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
+
+        });
+        */
+
+
+
+     }
 
 
     }
-}
+
+
+
+
